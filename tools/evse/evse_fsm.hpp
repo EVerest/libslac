f9efda5d89f38b9512ab40cbe3faf2d141fefdb3
Chandran <ch.goodchild@embedsmart.de>
2022-05-02 13:17:37 +0200

    switched to utc_clock
    Signed-off-by: Chandran <ch.goodchild@embedsmart.de>

--- tools/evse/evse_fsm.hpp
+++ tools/evse/evse_fsm.hpp
@@ -3,6 +3,8 @@
 #ifndef EVSE_FSM_HPP
 #define EVSE_FSM_HPP
 
+#include <date/date.h>
+#include <date/tz.h>
 #include <chrono>
 #include <string>
 
@@ -69,7 +71,7 @@ struct MatchingSessionContext {
     int captured_sounds{0};
     int captured_aags[slac::defs::AAG_LIST_LEN];
     bool received_mnbc_sound{false};
-    std::chrono::time_point<std::chrono::system_clock> tp_sound_start;
+    std::chrono::time_point<date::utc_clock> tp_sound_start;
 
     bool received_match_req{false};
 
